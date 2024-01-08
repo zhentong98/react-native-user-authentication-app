22d98ebef8616c1ee9c2778f45027a665de6fcc3
Chen Tong <chentong@xover.com.my>
2024-01-08 23:32:16 +0800

    Update

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 
-const API_KEY = '<Replace with your own API Key>'
+const API_KEY = '<Replace with your own API Key>';
+
 
 async function authenticate(mode, email, password) {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
